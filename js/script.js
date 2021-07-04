8c9df992708f16fc8d8e40cda990a7743915e4d6
Gianluca <gl.galassini@gmail.com>
2021-07-04 22:15:53 +0200

    chiamata axios

--- js/script.js
+++ js/script.js
@@ -2,7 +2,14 @@ const app = new Vue(
     {
         el: "#app",
         data: {
-            list: ["Pippo", "Pluto"]
+            list: []
+        },
+        mounted() {
+            axios
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then(x => {
+                        this.list.push(x.data.response)
+                })
         }
     }
 )
